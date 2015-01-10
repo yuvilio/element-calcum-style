88edab2ee9addf5d2260a6cd57c0b792f1bdf722
Yuval Maman <yuvilio@yahoo.com>
2015-01-09 19:12:20 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # element-calcum-style
-Calculate some data on an element and monitor it as you change a style in your browser's inspector
+Calculate some data item about elements you select. Refresh it's value when you change a style.
+
+## Example usage
+
+
+```html
+
+<div class="some-element">
+
+...
+</div>
+```
+
+```js
+var elCalcumStyle = require('element-calcum-style');
+
+elCalcumStyle({
+  selector: '.some-element p ',
+  label: 'height', //data-height
+  unit: 'px',
+  labelVisible: 1,
+  eventOnElem: document.querySelector('.some-element'),
+  style: 'height', //the one we'll affect in the inspector
+
+  callback: function(el){
+    return size(el)[1];
+  }
+});
+
+```
+
+Optional css to reveal the data change visually
+
+```css
+..some-element p /* the elements we've recalculated */
+{
+  &:before {
+    display: table;
+    bottom: 20px;
+    background-color: salmon;
+    color: white;
+    padding: 3px 2px;
+    right: 0;
+    content: attr(data-height);
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+//Calculate something about elements in a page and disiplayt (like their widths)
+var sliced = require('sliced')
+var MutationObserver = require('mutation-observer');
+
+
+var calcum = function(opts){
+  //go through all divs we want to calc and label
+  //carry out calculation and label them as an attribute
+  sliced(document.querySelectorAll(opts.selector)).forEach(function(element){
+
+    element.setAttribute(
+      'data-'+ opts.label,
+      (opts.labelVisible ?  opts.label + ':' : '') //show the label if they want to
+      + opts.callback(element)+opts.units
+    );
+  });
+};
+
+
+
+module.exports = function(opts){
+
+  //default options
+  var options = {};
+  options.selector = opts.selector || 'p[class*="col"],div[class*="col"]' ; //what elements to calculate and add data- attributes to
+
+  //the calculation we'll perform on an element
+  options.label = opts.label ||  'offsetWidth';
+
+  options.labelVisible = opts.labelVisible && 1; //default to label being visible unless specified
+  options.units = opts.units || 'px'; //default to 'px' being the unit unless specified (this is up to the client. the library knows nothing)
+
+  options.callback = opts.callback || function(el){
+    return el.offsetWidth;
+  }
+
+  //what event will trigure a recalculation? by default it's a 'resize' event on window
+  options.eventOnElem = opts.eventOnElem || window;
+  options.attribute = opts.attribute || 'style'; //by default, it'ss a style that will trigger the recalc (using your browser's element inspector)
+  options.style = opts.style || 'width'; //the style we'll watch for
+
+  //if they prefer a mutation observer
+
+  options.observerConfig = { childList: false, characterData: false,
+    attributes : true, attributeFilter : [ options.attribute ] };
+
+
+
+  calcum(options); //display first time (since this is called within a dom load)
+
+  //recalculate based on mutationobserver
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+
+      var styleAttr = mutation.target.getAttribute('style');
+      
+      //was it the style change  we're examining
+      if ( styleAttr.contains(options.style) ){
+        calcum(options);
+      }
+    });
+
+  });
+  observer.observe(options.eventOnElem, options.observerConfig);
+
+
+
+}
